77364b8e41eb0c9a7618eeedb4323d83e7023bc1
Lyubomyr Kovbel <shpintz@Lyubomyrs-MacBook-Pro.local>
2023-08-10 04:04:42 +0300

    something

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import Image from 'next/image'
 
 export default function Home() {
+
+  const gg = () => {
+    alert("SMile hoe")
+  }
+
+
   return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h2>hi</h2>
         <p>change me</p>
-        <button onClick={() => alert('Fiuck me')}> Click Me </button>
+        <button onClick={gg}> Click Me </button>
       </main>
     )
 }
