a372bfcf074baa0bbb0ddf58c0ea22177127c739
Lyubomyr Kovbel <shpintz@Lyubomyrs-MacBook-Pro.local>
2023-08-10 05:38:13 +0300

    small something

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,42 @@ import Image from 'next/image'
 
 export default function Home() {
 
-  const gg = () => {
-    alert("SMile hoe")
-  }
 
 
   return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h2>hi</h2>
-        <p>change me</p>
-        <button> Click Me </button>
+
+        {/* 
+          Header will have a seperate component
+          there will be a login opition & register option
+        */}
+        <div>
+          <nav>
+            <a href="#">Login</a>
+            <a href="#">Register</a>
+          </nav>
+        </div>
+
+        {/* 
+          There will be two sections divided within one another with a form content on the left and page data on the right
+        */}
+
+        <section>
+
+          {/* Content on the left */}
+          <div>
+
+            {/* Content about searching and creating an account */}
+            <div>
+              <input type="text" />
+              {/* logo / maginify glass */}
+            </div>
+
+          </div>
+
+
+        </section>
+
       </main>
     )
 }
