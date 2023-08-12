23df5384393b11d0b50a698f6cf68d35c5c33fbd
Lyubomyr Kovbel <shpintz@Lyubomyrs-MacBook-Pro.local>
2023-08-12 17:50:17 +0300

    small remodel 3x i think

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,39 @@
-// MainApp Index
+// layout.tsx
+"use client"
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { useState } from "react";
 
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import MainHeader from '@/components/mainHeader';
 
-import MainHeader from '@/components/mainHeader'
+import './globals.css';
 
-import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-  title: 'SocialChain',
-  description: 'The place to b e',
-}
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  const [address, setAddress] = useState(false);
 
+  const metamaskConnect = () => {
+      setAddress(prev => !prev);
+  };
+
+  const metadata: Metadata = {
+    title: 'SocialChain',
+    description: 'The place to b e',
+  };
 
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* Header  */}
-        <MainHeader />
-          {children}
+        {/* Header */}
+        <MainHeader metamaskConnect={metamaskConnect} address={address} />
+        {children}
         {/* Footer ? */}
-        </body>
+      </body>
     </html>
-  )
+  );
 }
--- components/mainHeader.tsx
+++ components/mainHeader.tsx
@@ -4,24 +4,49 @@ import Link from "next/link"
 import { Button } from "@/components/ui/button"
 
 
-const MainHeader = () => {
 
-    const metamaskConnect = () => {
-        console.log('metamask')
-    }
+interface MainHeaderProps{
+    metamaskConnect: () => void;
+    address: boolean; // change to string maybe
+}
+
+
+const MainHeader = ({metamaskConnect, address}: MainHeaderProps) => {
+
+
 
     return (
         <header id="MainHeader">
-            <h1>Socialchain</h1>
-            <nav>
-                <Link href="/About">About</Link>
-                <Link href="/AI">AI</Link>
-                <Button
-                    variant="outline" 
-                    onClick={metamaskConnect}>
-                    🦊
-                </Button>
-            </nav>
+            <div className="w-96 flex justify-between items-center">
+                <h1>
+                    <Link href="/dashboard">U</Link>
+                </h1>
+                <nav>
+                    <Link href="/About">About</Link>
+                    <Link href="/AI">AI</Link>
+                    {
+                        !address && (
+                        <Button
+                            variant="outline" 
+                            onClick={metamaskConnect}>
+                            🦊
+                        </Button>
+                        )
+                    }
+
+                </nav>
+            </div>
+            {
+                address ? (
+                    <div className="bg-[#35343e] p-2 rounded">
+                        Sat Aug 12 17:33
+                    </div>
+                ) : (
+                    <div className="bg-[#35343e] p-2 rounded">
+                        register
+                    </div>
+                )
+            }
         </header>
     )
 }
