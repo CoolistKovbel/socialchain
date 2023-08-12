491732cce384ff59300be9f2d78f05a8341eb380
Lyubomyr Kovbel <shpintz@Lyubomyrs-MacBook-Pro.local>
2023-08-12 05:10:51 +0300

    wait

--- app/(dashboard)/(routes)/profile/[slug]/page.tsx
+++ app/(dashboard)/(routes)/profile/[slug]/page.tsx
@@ -1,4 +1,6 @@
 
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 
 
@@ -6,14 +8,61 @@ import { useRouter } from 'next/router';
 
 const ProfilePage = ({ params }: { params: { slug: string } }) => {
 
-  console.log(params)
+  
+  const userData = {
+    user_name: params.slug,
+    user_image: "/bag-btc.png",
+    user_description: "i matter but i dont matter",
+    social_media: {
+      x: 'http://twitter.com/lyub12',
+      y: "http://youtube.com/lyub12",
+      z: "http://pintrest.com/lyub12"
+    },
+  }
+
+
   return (
     <div>
       <h1>User Profile</h1>
-      <p>Username: {params.slug}</p>
+
+      {/* Profile Card */}
+      <div>
+
+        {/* Profile image */}
+        <p>Username: {params.slug}</p>
+
+        {/* Profile image */}
+        <div className='relative w-64 h-64 '> 
+          <Image src={userData.user_image} alt="test" fill  />
+        </div>
+
+        {/* Social Menu */}
+        <nav>
+          <a href={userData.social_media.x}>👨🏽‍💻</a>
+          <a href={userData.social_media.y}>👩🏽‍💻</a>
+          <a href={userData.social_media.z}>🧑🏽‍💻</a>
+        </nav>
+
+        {/* Function Features */}
+        <div>
+          <Button>Recieve</Button>
+          <Button>Send</Button>
+          <Button>Poke</Button>
+          <Button>Message</Button>
+        </div>
+        
+      </div>
+
+      {/* Profile Comments */}
+      <div>
+        <h3>Comments: </h3>
+      </div>
+
     </div>
   );
 };
 
 export default ProfilePage;
+
+
 
